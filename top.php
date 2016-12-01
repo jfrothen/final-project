0b82f45b3fef0d7ef000e7c79aac7be9bf8e9777
jfrothen <jfrothen@uvm.edu>
2016-12-01 18:15:05 -0500

    Add files via upload

---
+++ top.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Final Project - A Study Guide</title>
+        
+        <meta charset="utf-8">
+        <meta name="author" content="Josh Rothenberg">
+        <meta name="description" content="html and php and studying" >
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+
+        
+    </head>
+    <body id="body">
+    
+        <?php include "header.php";
+        include "nav.php";
+        ?>
+
+
+
+        
+    
